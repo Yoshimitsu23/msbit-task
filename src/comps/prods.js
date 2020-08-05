731aa1a5070fc3e8318a0f0aa8e1ef80a1e78998
Elad Ahdut <elad_e_a@yahoo.com>
2020-08-05 22:30:57 +0300

    582020

--- src/comps/prods.js
+++ src/comps/prods.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { doApiGet } from '../service/apiService';
 
 
 function Prods() {
+
+    let [ prods_ar, setProds ] = useState([]);
+
+    useEffect(() => {
+        let url = "http://localhost:3000"
+
+        doApiGet(url)
+            .then(data => {
+                setProds(data)
+                console.log(data);
+            })
+    }, [])
+
     return (
         <div className='container'>
 
